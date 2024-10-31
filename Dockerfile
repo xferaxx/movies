164717f6e1ded88083761e4bc3c754516e70945f
xferaxx <juliankh18@hotmail.com>
2024-10-31 19:46:38 +0200

    new

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.11-slim
 WORKDIR /app/
 COPY requirements.txt .
+RUN apt-get update
+RUN apt-get install gcc default-libmysqlclient-dev -y
 RUN pip install -r requirements.txt
 COPY . .
 EXPOSE 8000
--- MovieDB/MovieDB/settings.py
+++ MovieDB/MovieDB/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,12 +73,16 @@ WSGI_APPLICATION = 'MovieDB.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, 'db.sqlite3')),
+        "USER": os.environ.get("SQL_USER", "myuser"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "myuserpassword"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "3306"),
     }
 }
-
+print(DATABASES)
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
